97c14a99b12e6c023a564e4c670f93ae51c7ea29
jplfalcao <jplinfo86@gmail.com>
2023-09-03 23:51:12 -0300

    Vagrant File

--- debian_vagrant_virtualbox/debian-default.sh
+++ debian_vagrant_virtualbox/debian-default.sh
@@ -2,8 +2,8 @@
 # Autor: João PauLo Falcão
 # Github: https://github.com/jplfalcao
 # Data de criação: 02/09/2023
-# Data de modificação:
-# Versão: 1.0
+# Data de modificação: 03/09/2023
+# Versão: 1.1
 
 # Atualizando o sistema e instalando alguns pacotes.
 apt-get update && apt-get upgrade -y
@@ -13,11 +13,11 @@ apt-get autoclean -y && sudo apt-get autoremove -y
 # Modificando o fuso horário.
 timedatectl set-timezone America/Recife
 
-# Adicionando aliases ao arquivo /etc/bash.bashrc.
+# Adicionando recursos ao `vim`, dentro do arquivo /etc/vim/vimrc.
 echo -e "syntax on\nset number\nset autoindent\nset tabstop=4\nset paste"\
 | tee -a /etc/vim/vimrc
 
-# Adicionando recursos ao `vim`, dentro do arquivo /etc/vim/vimrc.
+# Adicionando aliases ao arquivo /etc/bash.bashrc.
 echo -e "alias rm='rm -i'\nalias cp='cp -i'\nalias mv='mv -i'\nalias \
 grep='grep --color=auto'\nalias ls='ls --color=auto'\nalias vi='/usr/bin/vim'" \
 | tee -a /etc/bash.bashrc
